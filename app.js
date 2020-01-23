e277cd579e1629052715d7bc61e07e0ec913ccae
unknown <shabankhawar99@gmail.com>
2020-01-23 07:43:28 +0500

    if bcrypt doesnt work... try bcryptjs

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const dummy = [
+  {
+    username: "Dude",
+    title: "POST 1"
+  },
+  {
+    username: "Supreme",
+    title: "POST 2"
+  }
+];
 
 const app = express();
+
+//middleware
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+  res.json(dummy);
+});
+
+app.get("/login", (req, res) => {
+  //authenticate
+});
 
 app.listen(5000, () => {
   console.log("Server is live");
