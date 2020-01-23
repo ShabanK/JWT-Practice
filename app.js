6887836c4d5e78893e2321060e1c8deb89922886
unknown <shabankhawar99@gmail.com>
2020-01-23 17:54:18 +0500

    setting up bcyrpt

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const users = [];
 
 const dummy = [
   {
@@ -19,6 +22,39 @@ app.use(express.json());
 
 app.get("/posts", (req, res) => {
   res.json(dummy);
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const hashed = await bcrypt.hash(req.body.password, 10);
+    const user = { username: req.body.username, password: hashed };
+    users.push(user);
+    res.status(201).send("USER CREATED");
+  } catch {
+    res.status(500).json({ err: "Error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const username = req.body.username;
+      jwt.sign();
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.get("/login", (req, res) => {
