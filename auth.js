3dd67982202a1289a6081e5ad1d16f32a1393500
unknown <shabankhawar99@gmail.com>
2020-01-25 22:14:16 +0500

    cleaning up code

--- auth.js
+++ auth.js
@@ -43,19 +43,6 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) {
-    return res.sendStatus(401);
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
 app.get("/users", (req, res) => {
   res.json(users);
 });
